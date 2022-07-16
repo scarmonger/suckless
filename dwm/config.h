6f596df2980edc01329347ce4df0f5302c4474f5
scarmonger <psikomania@yahoo.com>
2022-07-16 16:20:43 +0700

    config

--- dwm/config.h
+++ dwm/config.h
@@ -2,9 +2,9 @@
 #include <X11/XF86keysym.h>
 
 /* Constants */
-#define TERMINAL "kitty"
-#define TERMCLASS "kitty"
-#define BROWSER "brave"
+#define TERMINAL "st"
+#define TERMCLASS "st"
+#define BROWSER "qutebrowser"
 
 /* appearance */
 static const char *light_up[] = {"/usr/bin/brightnessctl", "set", "5%+", NULL};
@@ -17,24 +17,17 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const unsigned int baralpha = 0xd0;
-static const unsigned int borderalpha = OPAQUE;
+static const char *fonts[]          = { "monospace:size=12","fontawesome=12" };
+static const char dmenufont[]       = "monospace:size=12";
+static const char col_gray1[]       = "#222222"; /** norm background color */
+static const char col_gray2[]       = "#444444"; /** norm border color */
+static const char col_gray3[]       = "#bbbbbb"; /** norm foreground color or font color */
+static const char col_gray4[]       = "#222222"; /** selected foreground color or font color original col : #eeeeee */
+static const char col_cyan[]        = "#86eb34"; /** selected background color, original col : #005577 */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-};
-static const unsigned int alphas[][3]      = {
-	/*               fg      bg        border     */
-	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
-	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
@@ -77,7 +70,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run_history", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "kitty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 
 static Key keys[] = {
