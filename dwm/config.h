9dd77b96ebfed3151cae27be9a7b8b1e61b715c0
scarmonger <psikomania@yahoo.com>
2023-01-06 10:29:24 +0700

    changer_border

--- dwm/config.h
+++ dwm/config.h
@@ -9,11 +9,11 @@
 /* appearance */
 /* static const char *light_up[] = {"/usr/bin/brightnessctl", "set", "5%+", NULL};
 static const char *light_down[] = {"/usr/bin/brightnessctl", "set", "5%-", NULL}; */
-static const char *upvol[] = { "/usr/bin/amixer", "set", "Master", "5%+", NULL };
-static const char *downvol[] = { "/usr/bin/amixer", "set", "Master", "5%-", NULL };
-static const char *mutevol[] = { "/usr/bin/amixer", "set", "Master", "toggle", NULL };
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const char *upvol[] = { "/usr/bin/amixer", "set", "Master", "5%+"};
+static const char *downvol[] = { "/usr/bin/amixer", "set", "Master", "5%-"};
+static const char *mutevol[] = { "/usr/bin/amixer", "set", "Master", "toggle"};
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int gappx     = 1;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
